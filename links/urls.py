45ffabe6ae4985eef8b35e8a1e2d87fd23d9d477
Antti <salosen.antti@gmail.com>
2011-05-31 00:23:03 +0300

    Fixes characters mauled by Posterous

--- links/urls.py
+++ links/urls.py
@@ -3,11 +3,11 @@ from links.views import *
 from models import Link
 
 urlpatterns = patterns('',
-    url(r'^new/$', new),
-    url(r'^add/$', add),
-    url(r'^$', list),
-    url(r'^(?P\d+)/$', view),
-    url(r'^(?P\d+)/edit/$', edit),
-    url(r'^(?P\d+)/update/$', update),
-    url(r'^(?P\d+)/delete/$', delete),
+  url(r'^new/$', new),
+  url(r'^add/$', add),
+  url(r'^$', list),
+  url(r'^(?P<id>\d+)/$', view),
+  url(r'^(?P<id>\d+)/edit/$', edit),
+  url(r'^(?P<id>\d+)/update/$', update),
+  url(r'^(?P<id>\d+)/delete/$', delete),
 )
