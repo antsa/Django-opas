43a9cea09da6a6ffdc4c741e594774d7ca04707d
Antti <salosen.antti@gmail.com>
2011-05-31 00:06:18 +0300

    Creates a model

--- links/models.py
+++ links/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Link (models.Model):
+    url = models.CharField(max_length=200)
+    title = models.CharField(max_length=50)
+    desc = models.CharField(max_length=140)
